58112cd68a41d124ad051fb24d4ea60f0f1d587b
keisuke071411 <i.love.football0714@gmail.com>
2023-08-14 10:33:53 +0900

    Add: Create new and create method

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -6,4 +6,23 @@ class TodoController < ApplicationController
   def show
     @todo = Todo.find(params[:id])
   end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      redirect_to @todo
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def todo_params
+      params.require(:todos).permit(:title, :description)
+    end
 end
